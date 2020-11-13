50834cdce22e0a13caeb05e3c28550ff584e020b
Goutham K <k.goutham2000@gmail.com>
2020-11-13 13:03:18 +0530

    raw details 1.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:ExpensY_APP/transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,14 +15,61 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 'exp1',
+      title: 'T-shirt',
+      amt: 150.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'exp2',
+      title: 'Shoes',
+      amt: 650.00,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('ExpensY'),
       ),
-      body: Center(
-        child: Text('Widget Area...'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('bar chart'),
+              color: Colors.blue,
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: transactions.map((trans) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text(
+                        trans.amt.toString(),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(trans.title),
+                        Text(trans.date.toString())
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
