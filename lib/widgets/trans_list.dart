79de1e38848f865bc9d08abe8837d3b0c9576585
Goutham K <k.goutham2000@gmail.com>
2020-11-15 12:41:07 +0530

    App with front-end(2) =>ListTile

--- lib/widgets/trans_list.dart
+++ lib/widgets/trans_list.dart
@@ -17,56 +17,51 @@ class TransationList extends StatelessWidget {
               children: [
                 Text(
                   "No Transactions Yet",
-                  style:TextStyle(fontSize: 18, color: Colors.black45 ),
+                  style: TextStyle(fontSize: 18, color: Colors.black45),
                 ),
-                SizedBox(height: 30,),
-                Container(height: 200,child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover,)),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    )),
               ],
             )
           : ListView.builder(
               // ignore: deprecated_member_use
               itemBuilder: (context, index) {
                 return Card(
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 3),
                   elevation: 3,
-                  child: Row(
-                    children: [
-                      Container(
-                        child: CircleAvatar(radius: 30,
-                          child: Padding(
-                            padding: const EdgeInsets.all(4),
-                            child: FittedBox(
-                              child: Text(
-                                '₹' + transactions[index].amt.toString(),
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                ),
-                              ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(4),
+                        child: FittedBox(
+                          child: Text(
+                            '₹' + transactions[index].amt.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Colors.white,
                             ),
                           ),
                         ),
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                        ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(transactions[index].title,
-                              style:
-                                  // TextStyle(fontSize: 16, fontWeight: FontWeight.bold,),
-                                  Theme.of(context)
-                                      .textTheme
-                                      .title // ignore: deprecated_member_use
-                              ),
-                          Text(
-                            DateFormat.yMMMEd()
-                                .format(transactions[index].date),
-                            style: TextStyle(color: Colors.grey),
-                          )
-                        ],
-                      )
-                    ],
+                      ),
+                    ),
+                    title: Text(transactions[index].title,
+                        style:
+                            // TextStyle(fontSize: 16, fontWeight: FontWeight.bold,),
+                            // ignore: deprecated_member_use
+                            Theme.of(context).textTheme.title),
+                    subtitle: Text(
+                      DateFormat.yMMMEd().format(transactions[index].date),
+                      style: TextStyle(color: Colors.grey),
+                    ),
                   ),
                 );
               },
